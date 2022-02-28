5aa77fc69c611f23843ea28302b532ee1a934df1
Kon-UU <92216445+Kon-UU@users.noreply.github.com>
2022-02-28 11:21:45 +0900

    Fixed Recursion error

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
 import hashlib
 import random
 import time
-from multiprocessing import Process, Queue
-
+from multiprocessing import Process
 THREADS = 4
 
 def program(cc):
-    TARGET = "3ba12a9bc2deba608d678a2a449ca7874c4df22588eb593e5150b6f3caf9f81d" #Game 5728987
-    STRING_POOL = "1234567890abcdef"
+    while True:
+        TARGET = "3ba12a9bc2deba608d678a2a449ca7874c4df22588eb593e5150b6f3caf9f81d" #Game 5728987
+        STRING_POOL = "1234567890abcdef"
 
-    generated_seed = ""
-    for i in range(64):
-        generated_seed += random.choice(STRING_POOL)
-    print("Start Searching at :", generated_seed)
+        generated_seed = ""
+        for i in range(64):
+            generated_seed += random.choice(STRING_POOL)
 
-    seed = generated_seed
+        print("Start Searching at :", generated_seed)
 
-    start = time.time()
+        seed = generated_seed
 
-    # Looperino starting here
-    for i in range(1000000):
+        start = time.time()
 
-        seed = hashlib.sha256(seed.encode()).hexdigest()
-        # print(seed)
+        for i in range(1000000):
 
-        if seed == TARGET:
-            print("Found Target !", seed, "at seed :", generated_seed, "at x st :", i)
-            end = time.time()
-            with open('hit.txt', 'a') as f:
-                f.write(f'\n{generated_seed} {i} {TARGET}')
-            print(f"It took {end - start}")
-            return
+            seed = hashlib.sha256(seed.encode()).hexdigest()
+            # print(seed)
 
-    end = time.time()
+            if seed == TARGET:
+                print("Found Target !", seed, "at seed :", generated_seed, "at x st :", i)
+                end = time.time()
+                with open('hit.txt', 'a') as f:
+                    f.write(f'\n{generated_seed} {i} {TARGET}')
+                print(f"It took {end - start}")
+                return
 
-    print(f"Failed at seed {generated_seed} and took {end - start} sec")
+        end = time.time()
 
-    program("c")
-
+        print(f"Failed at seed {generated_seed} and took {end - start} sec")
 
 if __name__ == '__main__':
     procs = []
@@ -45,6 +42,6 @@ if __name__ == '__main__':
         vysbufdi = Process(target=program, args=str(i))
         procs.append(vysbufdi)
         vysbufdi.start()
-
     for proc in procs:
         proc.join()
+        procs.remove(proc)
